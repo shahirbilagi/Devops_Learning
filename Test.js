e1e5499e4bf25596fcad15ca6056065dee169f38
shahirbilagi <shahirbilagi43@gmail.com>
2026-01-03 14:27:31 +0530

    feat: adding CI pipeline

---
+++ Test.js
@@ -0,0 +1,3 @@
+console.log("Starting time")
+setTimeout(() =>console.log('Waiting for 3 seconds...'), 3000);
+console.log("Ending time")
---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("Hello, Shahir")
+console.log("Welcome to DevOps Learning!")
+console.log("Let's explore CI/CD, Docker, and Kubernetes together!")
+console.log("Happy Coding!")
